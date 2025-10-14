5e80cf90da940f3146afd0a3ebf0d37cbbdcb082
Victor <victoreduardomartins2@gmail.com>
2025-10-14 19:02:20 -0300

    commitando alterações api
    a api de cumprimentos agora pode atualizar os valores que ja existem

--- rotas/japones.js
+++ rotas/japones.js
@@ -1,35 +1,34 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
 let japones = [
-    {id: 1, msg: "こんにちは"},    
-    {id: 2, msg: "おはようございます"},
-    {id: 3, msg: "おはよう"}
+  { id: 1, msg: "こんにちは" },
+  { id: 2, msg: "おはようございます" },
+  { id: 3, msg: "おはよう" },
 ];
 
-router.get("/",(req,res)=>{res.json(japones)})
+router.get("/", (req, res) => {
+  res.json(japones);
+});
 
-router.get("/:id",(req,res)=>{
-    
-    ///variavel de controle
-    let japa = "nao encontrei";
+router.get("/:id", (req, res) => {
+  ///variavel de controle
+  let japa = "nao encontrei";
 
-    //converter em inteiro
-    const id = parseInt(req.params.id); 
+  //converter em inteiro
+  const id = parseInt(req.params.id);
 
-    //inicializar o for
-    for(let i =0 ; i< japones.length; i++ ){
-
-        //criar a logica para encontrar a compri
-        if(japones[i].id == id){
-            japa = japones[i].msg;
-            break;
-        }
+  //inicializar o for
+  for (let i = 0; i < japones.length; i++) {
+    //criar a logica para encontrar a compri
+    if (japones[i].id == id) {
+      japa = { ...japones[i] };
+      return res.json(japones[i]);
     }
+  }
 
-    res.send(japa);
-
-})
+  res.json(japa);
+});
 // router.post("/", (req,res) => {
 
 //     const novoItem = [
@@ -44,13 +43,37 @@ router.get("/:id",(req,res)=>{
 //         // res.json(novoItem)
 // })
 router.post("/", (req, res) => {
-    for(let i = 0; i < req.body.length; i++){
-        let novoItem = {id: japones.length + 1, msg: req.body[i].msg}
-        japones.push(novoItem);
-    }
-    res.json(japones)
-    console.table(req.body)
-
+  for (let i = 0; i < req.body.length; i++) {
+    let novoItem = { id: japones.length + 1, msg: req.body[i].msg };
+    japones.push(novoItem);
+  }
+  res.json(japones);
+  console.table(req.body);
 });
+
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  for (let i = 0; i < japones.length; i++) {
+    if (japones[i].id === id) {
+      japones.splice(i, 1);
+      break;
+    }
+  }
+  res.json(japones);
+});
+
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const novoItem = req.body;
+  for (let i = 0; i < japones.length; i++) {
+    if (japones[i].id === id) {
+      japones[i] = { ...japones[i], ...novoItem };
+      return res.json(japones[i]);
+    }
+  }
+  res.json(japones);
+});
+
+
 
 module.exports = router;
